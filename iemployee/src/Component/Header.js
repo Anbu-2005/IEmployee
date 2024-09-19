1f8d85ccb53480df0cc1528e0c1d2fb5c972483a
bsaravanan783 <bsaravanan783@gmail.com>
2024-09-19 22:53:43 +0530

    tail config

--- iemployee/src/Component/Header.js
+++ iemployee/src/Component/Header.js
@@ -1,10 +1,7 @@
-import AddAlertIcon from "@mui/icons-material/AddAlert";
-import { AccessAlarm, ThreeDRotation } from "@mui/icons-material";
 
 const Header = () => {
   return (
     <div className="bg-blue-600 w-screen py-6">
-      <AccessAlarm />
       <h1 className="font-bold text-2xl">Header </h1>
     </div>
   );
